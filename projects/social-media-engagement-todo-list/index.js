1aaa48927af1b3b16f255671207e611fd2dba3b7
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-07-04 14:36:15 +0100

    Changed the code structure from procedural codes to Object Oriented Codes

--- projects/social-media-engagement-todo-list/index.js
+++ projects/social-media-engagement-todo-list/index.js
@@ -1,64 +1,5 @@
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-function modify(data) {
-    topics = [];
-    data.forEach(element => {
-        topics.push(element.topic)
-    });
-    
-    topics = shuffle(topics);
-    // console.log(topics);
-    i=0;
-    data.forEach(element => {
-        data[i].topic = topics[i];
-        ++i;
-        // console.log(data.topic);
-    });
-
-    return data;
-
-    console.log(data);
-}
-function display(data) {
-    content.innerHTML = null;
-    data = modify(data);
-    data.forEach(element => {
-        //this code is rough, I'd need to adjust it.
-        let row, column1, column2, text1, text2;
-        row = document.createElement('tr');
-        column1 = document.createElement('th');
-        text1 = document.createTextNode(element.day);
-        text2 = document.createTextNode(element.topic);
-        column1.setAttribute("scope", "row");
-        column1.setAttribute("style", "width:20%");
-        column1.appendChild(text1);
-        column2 = document.createElement('td');
-        column2.appendChild(text2);
-    
-        row.appendChild(column1);
-        row.appendChild(column2);
-        content.appendChild(row);
-        // console.log(element.day, "\n");
-    });
-    // data.length = 0;
-
-}
 let data = [
     {
         day: "Monday",
@@ -89,11 +30,89 @@ let data = [
         topic: "Invite people to come and join the service,Post links of church videos"
     }
 ]
-let content = document.getElementById('timetable');
 
 
-times = 0;
-setInterval(function() {
-    display(data);
+
+
+
+
+
+class DynamicTable {
+
+    //clone the table preset then set an enum
+    // if table has been initiated, add new table if not replace the table
+    constructor(data) {
+        this.data = data;
+    }
+
+    shuffle(data) {
+        var currentIndex = data.length, temporaryValue, randomIndex;
     
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+    
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+    
+            // And swap it with the current element.
+            temporaryValue = data[currentIndex];
+            data[currentIndex] = data[randomIndex];
+            data[randomIndex] = temporaryValue;
+        }
+    
+        return data;
+    }
+
+    modify(data) {
+        let topics = [];
+        data.forEach(element => {
+            topics.push(element.topic)
+        });
+        
+        topics = this.shuffle(topics);
+        let i=0;
+        data.forEach(element => {
+            data[i].topic = topics[i];
+            ++i;
+        });
+        this.data =data;
+    
+        return data;
+    
+    }
+    display() {
+        let content = document.getElementById('timetable');
+        content.innerHTML = null;
+        data = this.modify(this.data);
+        data.forEach(element => {
+            let row, column1, column2, text1, text2;
+            row = document.createElement('tr');
+            column1 = document.createElement('th');
+            text1 = document.createTextNode(element.day);
+            text2 = document.createTextNode(element.topic);
+            column1.setAttribute("scope", "row");
+            column1.setAttribute("style", "width:20%");
+            column1.appendChild(text1);
+            column2 = document.createElement('td');
+            column2.appendChild(text2);
+        
+            row.appendChild(column1);
+            row.appendChild(column2);
+            content.appendChild(row);
+        });
+    
+    }
+
+    
+
+    
+}
+
+  
+    
+test = new DynamicTable(data);
+setInterval(function() {
+    test.display();
 }, 5000);
+// test.display();
