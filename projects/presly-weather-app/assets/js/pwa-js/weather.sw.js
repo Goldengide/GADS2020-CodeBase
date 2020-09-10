08a8c85a0339da192cde7bc28bfee7a742854e7f
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-09-10 17:39:42 +0100

    update to my weather app

--- projects/presly-weather-app/assets/js/pwa-js/weather.sw.js
+++ projects/presly-weather-app/assets/js/pwa-js/weather.sw.js
@@ -2,7 +2,13 @@ self.addEventListener('install', event => {
     function onInstall() {
         return caches.open('static')
             .then(cache => cache.addAll([
-                ''
-            ]))
+                '/files/directory'
+            ]));
+            
     }
+    event.waitUntil(onInstall(event));
 });
+
+self.addEventListener('fetch', event => {
+    console.log('fetching water..');
+})
--- projects/presly-weather-app/assets/js/weather.api.js
+++ projects/presly-weather-app/assets/js/weather.api.js
@@ -3,24 +3,25 @@ searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
     cityName = document.getElementById('city');
 
-    data = {
-        'city': cityName,
+    cityData = {
+        'city': cityName.value,
         'appId': '247e7341e846c35a976a868d00ad47ec'
     }
     fetchWeatherAppApiByCity(cityData).then(data => {
         console.log(data);
+        // jsonData = JSON.stringify(data);
+        // console.log(jsonData);
         let insertPoint;
-        insertPoint = document.getElementById('weather')
-        // insertPoint.innerHTML = JSON.stringify(data.current.temp);
+        temperature = document.getElementById('temperature')
+        temperature.innerHTML = data.main.temp;
+        city = document.getElementById('city-name')
+        city.innerHTML = data.name + "<sup>" +data.sys.country + "</sup>";
     });
     console.log(cityName.value);
 
 })
 
-let cityData = {
-    'city': "Ile-Ife",
-    'appId': '247e7341e846c35a976a868d00ad47ec'
-}
+
 async function fetchWeatherAppApiByCity(data) {
 
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${data.appId}`);
