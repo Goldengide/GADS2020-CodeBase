bbfb5652169cdc6be435822daa43c5a7aa09b37b
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-09-11 16:43:24 +0100

    updated my weather app

--- projects/presly-weather-app/assets/js/weather.api.js
+++ projects/presly-weather-app/assets/js/weather.api.js
@@ -9,17 +9,23 @@ searchForm.addEventListener('submit', (event) => {
     }
     fetchWeatherAppApiByCity(cityData).then(data => {
         console.log(data);
-        // icon.src = "http://openweathermap.org/img/wn/04n@2x.png";
-        // icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-        // console.log(data.weather[0].icon);
-        // console.log(icon.src);
         icon = document.getElementById('icon');
         icon.innerHTML = `<img class="icon" src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt=""></img>`;
-        console.log(icon); 
         temperature = document.getElementById('temperature');
         temperature.innerHTML = data.main.temp;
         city = document.getElementById('city-name')
         city.innerHTML = data.name + "<sup>" +data.sys.country + "</sup>";
+        feels_like = document.getElementById('feels_like')
+        feels_like.innerHTML = data.main.feels_like;
+        humidity = document.getElementById('humidity')
+        humidity.innerHTML = data.main.humidity;
+        pressure = document.getElementById('pressure')
+        pressure.innerHTML = data.main.pressure;
+        wind = document.getElementById('wind')
+        wind.innerHTML = data.wind.speed;
+        description = document.getElementById('description')
+        description.innerHTML = data.weather.description;
+
     });
     console.log(cityName.value);
 
@@ -28,8 +34,12 @@ searchForm.addEventListener('submit', (event) => {
 
 
 async function fetchWeatherAppApiByCity(data) {
-
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${data.appId}`);
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${data.appId}`);
     let result = await response.json()
     return result;
+    } catch (error) {
+        console.log('Internet Hitch')
+    }
+    
 }
