66b55d3fc781dff5d3d0ea3149942bcfac36dbdd
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-09-14 17:01:43 +0100

    trying service worker functionality-trial-04

--- projects/presly-weather-app/weather.sw.js
+++ projects/presly-weather-app/weather.sw.js
@@ -4,6 +4,7 @@ const assets = [
     "/index.html",
     "/assets/css/theme.css",
     "/assets/css/template.css",
+    "/assets/js/init.js",
     "/assets/js/clock.js",
     "/assets/js/weather.api.js",
     "/assets/img/presley-weather-icon.png",
