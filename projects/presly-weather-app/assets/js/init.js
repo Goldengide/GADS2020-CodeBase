508bba40f33db236dfb4f52655e0eb9a061d5d16
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-09-14 14:08:49 +0100

    trying service worker functionality-trial-02

--- projects/presly-weather-app/assets/js/init.js
+++ projects/presly-weather-app/assets/js/init.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("../../weather.sw.js")
+        .register("weather.sw.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
