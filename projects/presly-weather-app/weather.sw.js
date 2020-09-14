b28d5af53f9e2dd485bd282d66283ff11871c1a2
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-09-14 14:16:09 +0100

    trying service worker functionality-trial-03

--- projects/presly-weather-app/weather.sw.js
+++ projects/presly-weather-app/weather.sw.js
@@ -1,22 +1,25 @@
-
 const staticDevWeather = "presley-weather-app";
 const assets = [
     "/index.html",
-    "assets/css/theme.css",
-    "assets/css/template.css",
-    "assets/js/init.js",
-    "assets/js/clock.js",
-    "assets/js/weather.api.js",
-    "assets/img/presley-weather-icon.png",
-    "assets/img/android-chrome-192x192.png",
-    "assets/img/android-chrome-512x512.png",
-    "assets/img/apple-touch-icon.png",
-    "assets/img/browserconfig.xml",
-    "assets/img/favicon.ico",
-    "assets/img/favicon-16x16.png",
-    "assets/img/favicon-32x32.png",
-    "assets/img/mstile-150x150.png",
-    "assets/img/safari-pinned-tab.svg",
+    "/assets/css/theme.css",
+    "/assets/css/template.css",
+    "/assets/js/init.js",
+    "/assets/js/clock.js",
+    "/assets/js/weather.api.js",
+    "/assets/img/presley-weather-icon.png",
+    "/assets/img/android-chrome-192x192.png",
+    "/assets/img/android-chrome-512x512.png",
+    "/assets/img/apple-touch-icon.png",
+    "/assets/img/browserconfig.xml",
+    "/assets/img/favicon.ico",
+    "/assets/img/favicon-16x16.png",
+    "/assets/img/favicon-32x32.png",
+    "/assets/img/mstile-150x150.png",
+    "/assets/img/safari-pinned-tab.svg",
+    "/vendor/fonts/axis/style.css",
+    "/vendor/fonts/axis/axis.woff",
+    "https://use.fontawesome.com/releases/v5.11.2/css/all.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/weather-icons/2.0.10/css/weather-icons.css"
 ]
 self.addEventListener("install", installEvent => {
     installEvent.waitUntil(
@@ -34,19 +37,3 @@ self.addEventListener("fetch", fetchEvent => {
     )
 });
 
-
-
-/* self.addEventListener('install', event => {
-    function onInstall() {
-        return caches.open('static')
-            .then(cache => cache.addAll([
-                '/files/directory'
-            ]));
-
-    }
-    event.waitUntil(onInstall(event));
-});
-
-self.addEventListener('fetch', event => {
-    console.log('fetching water..');
-}) */
